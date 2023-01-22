61e64c08a299bb80a2ef48c4405174977b27d0d8
tolbahamdi92 <tolba.hamdi92@gmail.com>
2023-01-22 21:32:59 +0300

    Configure app

---
+++ TodoList/Shared/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TodoList
+//
+//  Created by Tolba Hamdi on 1/22/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
